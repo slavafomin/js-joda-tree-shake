29b966018bae810789579b4858fb2717a1e3bce2
Slava Fomin II <slava@fomin.io>
2019-11-14 22:56:32 +0300

    Updated code to import from "@js-joda/core"

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ console.log(`Hello ${foo}!`);
 
 // Importing minimal symbol from the library
 // Tree shaking is not working for it, entire library is added to the bundle
-import { TextStyle } from 'js-joda';
+import { TextStyle } from '@js-joda/core';
 console.log(TextStyle.FULL);
